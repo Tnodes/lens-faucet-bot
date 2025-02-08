99b8740bdc21059706efe2979b8d25a1ee9e9b58
Tnodes <biel08468@gmail.com>
2025-02-08 17:56:42 +0700

    fix(banner): correct font name casing from Larry3D to larry3d to match pyfiglet font naming convention

--- src/utils/banner.py
+++ src/utils/banner.py
@@ -2,6 +2,6 @@ import pyfiglet
 import colorama 
 
 def print_banner():
-    print(pyfiglet.figlet_format("Tdrop", font="Larry3D", width=80))
+    print(pyfiglet.figlet_format("Tdrop", font="larry3d", width=80))
     print(colorama.Fore.GREEN + "Program Name     : Auto claim Lens Faucet" + colorama.Fore.RESET)
     print(colorama.Fore.GREEN + "Telegram Channel : https://t.me/tdropid" + colorama.Fore.RESET + "\n")
